ddf4c06509b35e3c6994c1b31471254136675ec4
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-09-23 02:08:20 +0300

    Complete Job Section Main Container

--- src/components/pages/JobPage/index.jsx
+++ src/components/pages/JobPage/index.jsx
@@ -1,52 +1,120 @@
-"use client";
-import { StyledFlex ,StyledBox ,StyledSidebar} from "@/styles/common";
+import React from "react";
+import styled from "styled-components";
+import { Avatar, Box, Stack, Divider, Slider } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import {
+  Heading,
+  MainHeading,
+  Body,
+  SmallText,
+} from "@/components/atoms/Typography/";
 import colors from "@/constants/colors";
-import { Avatar, Box, Stack, Typography, Divider, Slider } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import {StyledMainBox , StyleSideBox} from './style'
-import { Heading, MainHeading, Body ,SmallText } from "@/components/atoms/Typography/";
+import IconWithText from "./Components/Atoms/TextWithIcon";
+import WhereToVoteIcon from "@mui/icons-material/WhereToVote";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import SellIcon from "@mui/icons-material/Sell";
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SectionContainer from './Components/Atoms/SectionContainer'
+import Typography  from '@mui/material/Typography';
+import ChipsGrupe from '@/components/molecules/ChipsGrupe'
+const Container = styled(Box)`
+  border: 1px solid #ccc;
+  width: 100%;
+  border-radius: 10px;
+  display: flex;
+  row-gap: 30pd;
+`;
+3;
+const LeftBox = styled(Box)`
+  width: 75%;
+  border-right: 1px solid #ccc;
+  padding: 20px;
+`;
+
+const RightBox = styled(Box)`
+  width: 25%;
+  padding: 20px;
+`;
+
 
 
 const Job = () => {
-    const containerStyle = {
-        border: '1px solid #ccc',
-        width:'100%',
-        borderRadius: '10px', 
-      };
-    
-      const leftBoxStyle = {
-        width: '75%',
-        borderRight: '1px solid #ccc',
-      };
-    
-      const rightBoxStyle = {
-        width: '25%',
-      };
-    return (
-        <Box display="flex" style={containerStyle}>
-        <Box style={{ ...leftBoxStyle, padding: '20px' }}>
-          <Heading >
-          Configure NodeJS app with Google Auth
-          </Heading>
-          <Body hover>
-          Full Stack Development
-          </Body>
-          <SmallText color={colors.gray600Color}>
-          Posted 11 hours ago
-          </SmallText>
-          <Divider width='100%'/>
-          <Body>
-          I need someone to help me configure an existing webapp using ReactJS (frontend), NodeJS (backend), GoogleAPI (authentication) and MongoDB (database). Im trying to have it running in a VM (Azure) and to use a domain name.
-          </Body>
-        </Box>
+  return (
+    <Container>
+      <LeftBox>
+     
+        <Heading>Configure NodeJS app with Google Auth</Heading>
+        <Body hover>Full Stack Development</Body>
+        <SmallText color={colors.gray600Color}>Posted 11 hours ago</SmallText>
+        <IconWithText text={"Worldwide"} icon={<WhereToVoteIcon />} />
+        <Divider width="100%" />
+        <SectionContainer>
+        <Body>
+          I need someone to help me configure an existing webapp using ReactJS
+          (frontend), NodeJS (backend), GoogleAPI (authentication) and MongoDB
+          (database). I'm trying to have it running in a VM (Azure) and to use a
+          domain name.
+        </Body>
+        </SectionContainer>
+        <SectionContainer>
+        <Body fontWeight={"300"}>
+          Expensify is a team of generalists developing today's leading expense
+          management tool. Maintaining our reputation as an innovative leader in
+          the world of finance requires an incredibly reliable and secure system
+          for processing financial transactions. Accordingly, we primarily
+          leverage time-tested languages, but we're looking to unify our
+          front-end across platforms. For this, we're leveraging React Native
+          and are looking toward the community on Upwork to help solve a variety
+          of problems, both big and small, related to this migration.
+        </Body>
+        </SectionContainer>
+
+        <SectionContainer>
+        <Stack direction="row" spacing={1}>
+          <ListItem sx={{maxWidth:'220px'}} >
+            <SellIcon fontSize="small" sx={{ marginRight: "8px" }} />
+            <ListItemText primary="$100.00" secondary="Fixed-price" />
+          </ListItem>
+
+          <ListItem sx={{maxWidth:'220px'}}>
+            <PsychologyIcon fontSize="small" sx={{ marginRight: "8px" }} />
+            <ListItemText primary="Intermediate" secondary="I am looking for a mix of experience and value" />
+          </ListItem>
+        </Stack>
+        </SectionContainer>
         
+        <SectionContainer>
+        <Typography variant="body2" gutterBottom>
+         Project Type:  One-time project  
+        </Typography>
+        </SectionContainer>
+
+        <SectionContainer>
+        <Typography variant="h6" >
+        Skills and Expertise
+        </Typography>
+        <ChipsGrupe list={['react' ,'node js']}/>
+        </SectionContainer>
+
+        <SectionContainer>
+        <Typography variant="h6" >
+        Activity on this job
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+        Proposals:  15 to 20
+        </Typography>   
+        </SectionContainer>
+
+      </LeftBox>
 
 
+      <RightBox>
+      
+      </RightBox>
+    </Container>
+  );
+};
 
-        <Box style={{ ...rightBoxStyle, padding: '20px' }}>
- 
-        </Box>
-      </Box>
-    )
-    }
 export default Job;

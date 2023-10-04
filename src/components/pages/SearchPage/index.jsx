296c8b5050a2fdf2cce042c6acfda5ec0339d235
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-10-05 02:32:57 +0300

    create search result box
    todo : set the width to 100% on saved jobs

--- src/components/pages/SearchPage/index.jsx
+++ src/components/pages/SearchPage/index.jsx
@@ -1,10 +1,18 @@
 "use client";
 import SideBar from './components/SideBar';
 import PageContainer from './components/PageContainer'
-import { Box, styled } from "@mui/material";
+import { Box, Stack, styled } from "@mui/material";
 import { StyledFlex,StyledBox,StyledSidebar } from "@/styles/common";
 import { AntTabs, AntTab } from "@/components/molecules/Tabs";
 import React, { useState } from "react";
+import Search from '../HomePage/components/molecules/SearchBar';
+import Button from "@mui/material-next/Button";
+import FolderIcon from '@mui/icons-material/Folder';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import Typography from "@mui/material/Typography";
+import jobData from "@/db/jobs";
+import JobCard from "@/components/molecules/JopCard";
+
 
 const StyledSearchPageContainer=styled(Box)`
    display: flex;
@@ -30,7 +38,7 @@ const StyledSearchPageContainer=styled(Box)`
   }
 
 `
-
+import {SaveJobButtonStyle} from '@/components/pages/JobPage/style';
 const StyledSideBarContainer=styled(Box)`
   width:25%;
   padding :15px;
@@ -40,8 +48,35 @@ const StyledSearchContainer=styled(Box)`
   width:75%;
   padding :15px;
 `
-const SearchComponent = () => <div>This is the Search component content</div>;
-const SavedJobsComponent = () => <div>This is the Saved Jobs component content</div>;
+const SearchComponent = () => (
+  
+  <Stack sx={{padding:'18px 8px'}}>
+    <Box style={{display: 'flex',flexDirection:'row'}}>    
+    <Search width={'76%'} />
+    <Button  style={{...SaveJobButtonStyle , width:'130px' }} variant="outlined" >
+    <FolderIcon/>
+      Saved Jobs
+    </Button>
+    </Box>
+    <Box style={{display: 'flex',flexDirection:'row' , marginBottom:'14px'}}>
+    <RssFeedIcon/>
+    <Typography color='primary'>1500 found jobs </Typography>
+    </Box>
+    {jobData.map((job) => (
+        <JobCard key={job.jobsId} {...job} />
+      ))}
+  </Stack>
+  
+)
+const SavedJobsComponent = () =>{
+  return(
+    <>
+       {jobData.map((job) => (
+        <JobCard key={job.jobsId} {...job} />
+      ))}
+    </>
+  )
+}
 
 const SearchPage = () => {
   const tabsData = [
@@ -70,7 +105,7 @@ const SearchPage = () => {
         <SideBar/>
         </StyledSideBarContainer>
         <StyledSearchContainer>
-        <StyledBox >
+        <StyledBox padding ='14px'>
         <AntTabs value={activeTab} onChange={handleTabChange}>
             {tabsData.map((tab) => (
               <AntTab key={tab.value} value={tab.value} label={tab.label}  />

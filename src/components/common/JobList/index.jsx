61070ebef10e5e63b0ccd6f654770d6d8bd40d2d
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-10-05 15:04:17 +0300

    Create Job List Component

---
+++ src/components/common/JobList/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import JobCard from "@/components/molecules/JopCard";
+
+const JobList = ({ jobData, handleDrawerOpen }) => {
+  if (!jobData || jobData.length === 0) {
+    return <p>No jobs found yet.</p>;
+  }
+
+  return (
+    <div>
+      {jobData.map((job) => (
+        <JobCard key={job.jobsId} {...job} onClick={() => handleDrawerOpen(job)} />
+      ))}
+    </div>
+  );
+};
+
+export default JobList;

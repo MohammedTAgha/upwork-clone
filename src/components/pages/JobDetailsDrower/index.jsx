02edcb2ddb002f12a2f347dd052b1eda6bc1732c
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-10-05 15:33:48 +0300

    update drawer : add top icon and link

--- src/components/pages/JobDetailsDrower/index.jsx
+++ src/components/pages/JobDetailsDrower/index.jsx
@@ -1,15 +1,37 @@
-import Typography from '@mui/material/Typography';
-import JobPage from '@/components/pages/JobPage'
-const JobDetailsDrower = ({job}) => {
-    return (
-        <div>
-    <Typography variant="h4" gutterBottom>
-        Job Diatails 
-      </Typography>
-        <JobPage/>
-        </div>
-    )
-
-}
+import Typography from "@mui/material/Typography";
+import JobPage from "@/components/pages/JobPage";
+import { Box } from "@mui/material";
+import { Body } from "@/components/atoms/Typography/";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+const JobDetailsDrower = ({ job }) => {
+  return (
+    <div>
+      <Box
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          margin: "8px 0 48px 0",
+        }}
+      >
+        <ArrowBackIosIcon />
+        <Box
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "row-reverse",
+            alignItems: "center",
+          }}
+        >
+          <Body hover>
+            <OpenInNewIcon />
+             Open job in new window
+          </Body>
+        </Box>
+      </Box>
+      <JobPage />
+    </div>
+  );
+};
 
 export default JobDetailsDrower;

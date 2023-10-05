57955aa836879d66f3b2e1ce9f276b77ea3d7106
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-10-05 19:06:13 +0300

    Update useApi.js

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useJobData = (searchQuery = '') => {
+const useJobData = (searchQuery = '', jobId = null) => {
   const [jobData, setJobData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -9,14 +9,25 @@ const useJobData = (searchQuery = '') => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/jobs');  // Adjust URL as needed
-
-        // Filter jobs based on the search query
-        const filteredJobs = response.data.filter(job =>
-          job.title.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-
-        setJobData(filteredJobs);
+        let response;
+          // Fetch all jobs and filter based on the search query
+          response = await axios.get('http://localhost:5000/jobs');
+          let jobs = response.data;
+  
+          // If jobId is provided, filter the jobs to get the specific job
+          if (jobId) {
+            console.log('id exist')
+            const job = jobs.find(job => job.jobsId == jobId);  
+            setJobData(job ? [job] : []);
+          } else {
+            console.log('id no')
+            // Filter jobs based on the search query
+            const filteredJobs = jobs.filter(job =>
+              job.title.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setJobData(filteredJobs);
+          }
+        
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -25,8 +36,7 @@ const useJobData = (searchQuery = '') => {
     };
 
     fetchData();
-  }, [searchQuery]);
-
+  }, [searchQuery, jobId]);
   return { jobData, loading, error };
 };
 

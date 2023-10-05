32e8712781a779e65122d8e981a156c52bc7e9d3
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-10-05 15:10:54 +0300

    add search to use Api

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useJobData = () => {
+const useJobData = (searchQuery = '') => {
   const [jobData, setJobData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -10,7 +10,13 @@ const useJobData = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:5000/jobs');  // Adjust URL as needed
-        setJobData(response.data);
+
+        // Filter jobs based on the search query
+        const filteredJobs = response.data.filter(job =>
+          job.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        setJobData(filteredJobs);
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -19,7 +25,7 @@ const useJobData = () => {
     };
 
     fetchData();
-  }, []);
+  }, [searchQuery]);
 
   return { jobData, loading, error };
 };
